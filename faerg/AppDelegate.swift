2b5defdef7590c3f81692d75557b3850e6b16309
Harry Lundström <h.lundstroem@gmail.com>
2018-02-18 11:33:06 +0100

    trying out document actions and file dialog

--- faerg/AppDelegate.swift
+++ faerg/AppDelegate.swift
@@ -21,6 +21,39 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
+    @IBAction func newDocument(_ sender: Any?) {
 
+    }
+    
+    @IBAction func openDocument(_ sender: Any?) {
+        browseFile(sender: self)
+    }
+    
+    func browseFile(sender: AnyObject) {
+        
+        let dialog = NSOpenPanel();
+        
+        dialog.title                   = "Choose a png or jpg file";
+        dialog.showsResizeIndicator    = true;
+        dialog.showsHiddenFiles        = false;
+        dialog.canChooseDirectories    = true;
+        dialog.canCreateDirectories    = true;
+        dialog.allowsMultipleSelection = false;
+        dialog.allowedFileTypes        = ["png","jpg"];
+        
+        if (dialog.runModal() == NSApplication.ModalResponse.OK) {
+            let result = dialog.url // Pathname of the file
+            
+            if (result != nil) {
+                let path = result!.path
+                //filename_field.stringValue = path
+                print("path:", path)
+            }
+        } else {
+            // User clicked on "Cancel"
+            return
+        }
+        
+    }
 }
 
