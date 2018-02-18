f5b42939d925671316c33fdab2264513be9c5916
Harry Lundström <h.lundstroem@gmail.com>
2018-02-18 10:50:04 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# faerg
+The goal of faerg is to be a very simple image editing application for macOS, reminiscent of MSPaint on Windows XP.
---
+++ faerg/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  faerg
+//
+//  Created by Harry Lundstrom on 2018-02-18.
+//  Copyright © 2018 Harry Lundström. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
